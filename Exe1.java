cb7d0f9c46ec6deea68cdc7297a5939a4aa053b1
42lex <36453304+42lex@users.noreply.github.com>
2021-10-06 15:26:42 +0200

    Add files via upload
    Upload des fichier

---
+++ Exe1.java
@@ -0,0 +1,24 @@
+package tp2;
+
+public class Exe1 {
+	//Précision et variables de type double
+
+	public static void main(String[] args) {
+		
+	double x=0; 	//Initalisation de la variable x à 0 
+				// Mise de la valeur X à 0 
+	
+	for(int i=0;i<10;i++) {
+		
+		x+=0.1;
+		
+		System.out.println(x); //Afficher le résultat 
+
+	}
+	
+	
+
+
+	}
+
+}
---
+++ Exe2.java
@@ -0,0 +1,24 @@
+package tp2;
+
+public class Exe2 {
+	//Conversion degrés Fahrenheit vers Celsius
+	//On vas afficher la table de conversion pour le coup entre 
+	//Le degré celsius et le Fareinheint
+	//Calculer de 250 à -20 
+	
+	public static void main(String[] args) {
+		
+		int x=0; 	//Initalisation de la variable x à 0 
+		// Mise de la valeur X à 0 
+
+for(int i=250;i>=-20;i=i-10) {
+
+x=((5/9)*x-160/9);                                    
+
+System.out.println(x); //Afficher le résultat 
+
+		}
+	}
+
+}
+
